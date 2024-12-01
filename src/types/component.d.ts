6267a1ae0fe0f7263ab4c8d1edf5ee26d8d4bb8d
uuyo <2817799342@qq.com>
2024-12-01 21:40:56 +0800

    feat: 添加“猜你喜欢”组件并更新首页布局

--- src/types/component.d.ts
+++ src/types/component.d.ts
@@ -4,10 +4,12 @@
  * declare module 'vue'
  */
 import XtxSwiper from '@/components/XtxSwiper.vue'
+import XtxGuess from '@/components/Xtxguess.vue'
 import 'vue'
 declare module 'vue' {
   export interface GlobalComponents {
     XtxSwiper: typeof XtxSwiper
+    XtxGuess: typeof XtxGuess
     //
   }
 }
